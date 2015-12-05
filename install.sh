fe28973a1fa7beabf82d0fcb3974ff7974d3d90f
Tristan de Lataillade <delataillade.tristan@gmail.com>
2015-12-05 15:35:58 +0000

    fixes in install script: skipping sections when reinstalling

--- install.sh
+++ install.sh
@@ -14,21 +14,26 @@ if [ ! -d ${BASEDIR}/.vim/bundle ]
 then
     mkdir ${BASEDIR}/.vim
     mkdir ${BASEDIR}/.vim/bundle
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    git clone https://github.com/VundleVim/Vundle.vim.git ${BASEDIR}/.vim/bundle/Vundle.vim
     vim +PluginInstall +qall
 fi
 # install plugins through vundle
 
 # ---------- zsh ----------
-if [ ! -d ${BASEDIR}/powerline ]
+if [ ! -d ${BASEDIR}/.antigen ]
 then
-    git clone https://github.com/powerline/fonts
+    git clone https://github.com/powerline/fonts ${BASEDIR}/.antigen
+    ln -sfn ${BASEDIR}/.antigen/ ~/.antigen
+fi
+if [ ! -d ${BASEDIR}/fonts ]
+then
+    git clone https://github.com/powerline/fonts ${BASEDIR}/fonts
     ${BASEDIR}/fonts/install.sh
 fi
 if [ ! -d ${BASEDIR}/gnome-terminal-colors-solarized ]
 then
-    git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
-    gnome-terminal-colors-solarized/install.sh
+    git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git ${BASEDIR}/gnome-terminal-colors-solarized
+    ${BASEDIR}/gnome-terminal-colors-solarized/install.sh
     gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "Ubuntu Mono derivative Powerline 12"
 fi
 # chsh -s /bin/zsh
